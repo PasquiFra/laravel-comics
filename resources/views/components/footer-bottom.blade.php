ce533ea649171abbb27612def5866f27797d4297
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-08 10:08:41 +0100

    backgrounds fixed

--- resources/views/components/footer-bottom.blade.php
+++ resources/views/components/footer-bottom.blade.php
@@ -12,11 +12,11 @@
                 <li>
                     <h3>FOLLOW US</h3>
                 </li>
-                <li><a href="#"><img src="../assets/img/footer-facebook.png" alt="facebook"></a></li>
-                <li><a href="#"><img src="../assets/img/footer-twitter.png" alt="twitter"></a></li>
-                <li><a href="#"><img src="../assets/img/footer-youtube.png" alt="youtube"></a></li>
-                <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt="pinterest"></a></li>
-                <li><a href="#"><img src="../assets/img/footer-periscope.png" alt="periscope"></a></li>
+                <li><a href="#"><img src="{{Vite::asset('/resources/img/footer-facebook.png')}}" alt="facebook"></a></li>
+                <li><a href="#"><img src="{{Vite::asset('/resources/img/footer-twitter.png')}}" alt="twitter"></a></li>
+                <li><a href="#"><img src="{{Vite::asset('/resources/img/footer-youtube.png')}}" alt="youtube"></a></li>
+                <li><a href="#"><img src="{{Vite::asset('/resources/img/footer-pinterest.png')}}" alt="pinterest"></a></li>
+                <li><a href="#"><img src="{{Vite::asset('/resources/img/footer-periscope.png')}}" alt="periscope"></a></li>
             </ul>
         </nav>
     </div>
