b5fc8cfb114e595c9fc7609100599ebdd89890e0
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-09 16:59:35 +0100

    css

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -16,11 +16,53 @@
             <div class="flex space-between">
                 <div id="comic-content">
                     <h2>{{$comic['title']}}</h2>
-                    <div id="info"></div>
+                    <div id="info" class="flex space-between">
+                        <div class="price flex space-between">
+                            <div>
+                                U.S. Price: <span>{{$comic['price']}}</span>
+                            </div>
+                            <div>AVAILABLE</div>
+                        </div>
+                        <div class="available flex center">
+                            <div class="check-available">Check availability</div>
+                        </div>
+                    </div>
                     <p> {{$comic['description']}} </p>
                 </div>
                 <div id="adv">
                     <img src=" {{Vite::asset('/resources/img/adv.jpg')}} " alt="DC POWER VISA">
+                </div>
+            </div>
+            <div class="flex space-between">
+                <div>
+                    <h3>Talent</h3>
+                    <ul>
+                        Art by:
+                        <li>
+                        
+                        </li>
+                    </ul>
+                    <ul>
+                        Written by:
+                        <li>
+                        
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Specs</h3>
+                    <ul>
+                        Series:
+                        <li>
+                        
+                        </li>
+                    </ul>
+                    <div>
+                        U.S. Price: <span>{{$comic['price']}}</span>
+                    </div>
+                    <div>
+                        On Sale Date: 
+                    </div>
                 </div>
             </div>
             
